6f7fc81d9e74496773c99dd449bfc673b3e7732e
Steven Zeck <steven.zeck@gmail.com>
2018-02-21 21:12:08 -0600

    Fix lint errors

--- test/languages/java.spec.ts
+++ test/languages/java.spec.ts
@@ -3,8 +3,9 @@ import beautifier from "../../src";
 test(`should successfully beautify Java text without breaking chained methods`, () => {
   const unibeautify = newUnibeautify();
   unibeautify.loadBeautifier(beautifier);
-    const text = `public class Foo{public static void Main(String[] args){System.out.println("Hello World!");}}`;
-  const beautifierResult = `public class Foo {\n\tpublic static void Main(String[] args) {\n\t\tSystem.out.println("Hello World!");\n\t}\n}`;
+  const text = `public class Foo{public static void Main(String[] args){System.out.println("Hello World!");}}`;
+  const beautifierResult = `public class Foo {\n\tpublic static void Main(String[] args) {\n\t\t\
+System.out.println("Hello World!");\n\t}\n}`;
   return unibeautify
     .beautify({
       languageName: "Java",
